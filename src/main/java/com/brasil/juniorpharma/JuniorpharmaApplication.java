3278a67c1d2aaef1b4b99383c0f314419c7b57c9
ArkimedesJunior <arkimedesjunior01@gmail.com>
2025-01-27 10:07:25 -0300

    configuração inicial

---
+++ src/main/java/com/brasil/juniorpharma/JuniorpharmaApplication.java
@@ -0,0 +1,13 @@
+package com.brasil.juniorpharma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniorpharmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuniorpharmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brasil/juniorpharma/JuniorpharmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brasil.juniorpharma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuniorpharmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
